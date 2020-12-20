71694fc81acb32df8d200dd83b412d88808be9e1
sony-zloi <31725936+sony-zloi@users.noreply.github.com>
2020-12-20 13:04:55 +0500

    prerelease version

--- task_1.py
+++ task_1.py
@@ -12,20 +12,37 @@ class Auto:
 
         Auto.carList.append(self.carProp)
 
-
     @staticmethod
     def showData():
         print(f'The properties of the car are: ')
         for element in Auto.carList:
             for keys, values in element.items():
                 print(keys + ": " + values)
-
-    def addData(self):
-        self.carProp['name'] = input('Name: ')
-        self.carProp['year'] = str(input('Year: '))
+            print()
 
 
-nameOfObj1 = Auto()
-nameOfObj2 = Auto()
-carList = Auto()
-carList.showData()
+def userEnter():
+    enter = input('Имеется класс \"Автомобиль\" \n'
+                  'Для ввода нажмите 1: \n'
+                  'Для вывода нажмте 2: \n'
+                  'Для выхода нажмите любую другую клавишу:\n'
+                  '> ')
+    return enter
+
+
+while True:
+    counter = 0
+    enter = userEnter()
+
+    if enter == '1':
+        counter += 1
+        nameOfObj = 'car' + str(counter)
+        print(f'Enter the {nameOfObj} properties:')
+        nameOfObj = Auto()
+
+    elif enter == '2':
+        Auto.showData()
+    else:
+        break
+
+print('Bye!')
